0e66ccd0c235fe4d440dd7379de20059b77c2703
DamianoL <damianolunardo@hotmail.it>
2023-11-10 16:22:46 +0100

    ADD functionality for API movies

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,8 @@
 <script>
 import AppCard from './AppCard.vue';
 import AppCardSeries from './AppCardSeries.vue';
+import axios from 'axios';
+import { store } from '../store';
 
 
 export default {
@@ -9,7 +11,27 @@ export default {
         AppCard,
         AppCardSeries
     },
-   
+    data() {
+        return {
+            store
+        }
+    },
+   methods: {
+    fetchMovies() {
+        axios.get ('https://api.themoviedb.org/3/search/movie',{
+            params: {
+                api_key: this.store.API_KEY,
+                query: this.store.query
+            }
+        }).then (res => {
+            console.log(res.data.results)
+        })
+    }
+   },
+    created() {
+        this.fetchMovies(),
+        console.log('Created')
+    }
 }
 </script>
 
---
+++ src/store.js
@@ -0,0 +1,7 @@
+import { reactive } from "vue";
+
+
+export const store = reactive({
+    API_KEY: 'f2e1a693b0773ba38af36cd5791129f1',
+    query: 'star',
+})
