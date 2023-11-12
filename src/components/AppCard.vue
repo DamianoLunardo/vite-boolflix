c0e61be0860c3462f3244cd7b2bfb60f48322a70
DamianoL <damianolunardo@hotmail.it>
2023-11-12 11:58:35 +0100

    ADD film poster image

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -10,25 +10,25 @@ export default {
     },
   },
 };
-
-
 </script>
 
 <template>
-    <div class="card__film">
-      <div>{{ movie.title }}</div>
-      <div>{{ movie.original_title }}</div>
-      <div :class="flagClass"></div>
-      <div>{{ movie.vote_average }}</div>
+  <div class="card__film">
+    <div>
+      <img :src="`https://image.tmdb.org/t/p/w342/${movie.poster_path}`" />
     </div>
-  </template>
+    <div>{{ movie.title }}</div>
+    <div>{{ movie.original_title }}</div>
+    <div :class="flagClass"></div>
+    <div>{{ movie.vote_average }}</div>
+  </div>
+</template>
 
 <style lang="scss">
-@import url('https://cdn.jsdelivr.net/npm/flag-icon-css@3.5.0/css/flag-icon.min.css');
+@import url("https://cdn.jsdelivr.net/npm/flag-icon-css@3.5.0/css/flag-icon.min.css");
 .card__film {
   border: 1px solid #ccc;
   padding: 10px;
   margin-bottom: 10px;
 }
-
 </style>
