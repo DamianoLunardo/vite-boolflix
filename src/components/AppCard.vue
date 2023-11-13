6e66f30ee2a134a4e7277d855fcccbd7bbfe2ea5
DamianoL <damianolunardo@hotmail.it>
2023-11-13 13:05:29 +0100

    ADD stars count

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -20,15 +20,27 @@ export default {
     <div>{{ movie.title }}</div>
     <div>{{ movie.original_title }}</div>
     <div :class="flagClass"></div>
-    <div>{{ movie.vote_average }}</div>
+    <div class="star-ratings">
+      <template v-for="i in Math.floor(movie.vote_average / 2)">
+        <i class="fas fa-star"></i>
+      </template>
+      <template v-if="movie.vote_average % 2 !== 0">
+        <i class="fas fa-star-half"></i>
+      </template>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
 @import url("https://cdn.jsdelivr.net/npm/flag-icon-css@3.5.0/css/flag-icon.min.css");
+@import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css");
 .card__film {
   border: 1px solid #ccc;
   padding: 10px;
   margin-bottom: 10px;
 }
+
+.star-ratings {
+    color: rgb(251, 255, 0);
+  }
 </style>
