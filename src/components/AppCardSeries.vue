92bed4d4eec841a0727da640dd2a3175614ab1ba
DamianoL <damianolunardo@hotmail.it>
2023-11-13 11:29:37 +0100

    Remove commented code

--- src/components/AppCardSeries.vue
+++ src/components/AppCardSeries.vue
@@ -1,24 +1,20 @@
 <script>
-//export default {
-  //props: {
-   
-   // serie : Object,
-  //},
- // computed: {
-  
-   // flagClassTv() {
-   //  const language = this.serie.original_language;
-    // return `flag-icon flag-icon-${language}`;
-   // },
- // },
-//};
+export default {
+  props: {
+
+    serie: Object,
+  },
+  computed: {
+flagClassTv() {
+      const language = this.serie.original_language;
+      return `flag-icon flag-icon-${language}`;
+    },
+  },
+};
 </script>
 
 <template>
- 
-
-
-  <!--  <div class="card__serie" >
+  <div class="card__serie">
     <div>
       <img :src="`https://image.tmdb.org/t/p/w342/${serie.backdrop_path}`" />
     </div>
@@ -27,11 +23,11 @@
     <div :class="flagClassTv"></div>
     <div>{{ serie.popularity }}</div>
   </div>
--->
 </template>
 
 <style lang="scss">
 @import url("https://cdn.jsdelivr.net/npm/flag-icon-css@3.5.0/css/flag-icon.min.css");
+
 .card__serie {
   border: 1px solid #ccc;
   padding: 10px;
