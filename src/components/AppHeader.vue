6f33bd468531d6e57e18722a30085155d430abbc
DamianoL <damianolunardo@hotmail.it>
2023-11-13 17:43:07 +0100

    ADD header option

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,5 @@
 <script>
-import { store } from '../store';
+import { store } from "../store";
 
 export default {
   data() {
@@ -7,24 +7,32 @@ export default {
       store,
     };
   },
-  
-  
 };
 </script>
 
-<template> 
+<template>
   <div class="container">
     <div class="header">
       <div class="title">BOOLFLIX</div>
+      <div class="nav-bar">
+        <ul class="nav-list">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Serie TV</a></li>
+          <li><a href="#">Film</a></li>
+          <li><a href="#">Originali</a></li>
+          <li><a href="#">Aggiunti di recente</a></li>
+          <li><a href="#">La mia lista</a></li>
+        </ul>
+      </div>
       <div>
         <input v-model="store.query" type="text" placeholder="Cerca..." />
-        
       </div>
     </div>
   </div>
 </template>
 
 <style lang="scss">
+
 .header {
   display: flex;
   justify-content: space-between;
@@ -36,4 +44,22 @@ export default {
   font-size: 30px;
   font-weight: bold;
 }
+.nav-list {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  text-align: center;
+}
+
+a {
+  text-decoration: none;
+  color: white;
+  padding: 0 5px;
+  font-weight: bold;
+  font-size: 10px;
+  &:hover {
+    color: red;
+}
+}
+
 </style>
