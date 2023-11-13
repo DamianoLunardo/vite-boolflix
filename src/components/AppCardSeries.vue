90e1cc8ddfbcd5f6ac7beeca350247e511c81d7b
DamianoL <damianolunardo@hotmail.it>
2023-11-13 15:53:25 +0100

    ADD hover for tv series

--- src/components/AppCardSeries.vue
+++ src/components/AppCardSeries.vue
@@ -4,6 +4,11 @@ export default {
 
     serie: Object,
   },
+  data() {
+    return {
+      showInfo: false,
+    };
+  },
   computed: {
 flagClassTv() {
       const language = this.serie.original_language;
@@ -14,13 +19,18 @@ flagClassTv() {
 </script>
 
 <template>
-  <div class="card__serie">
+  <div class="card__serie"  @mouseover="showInfo = true" @mouseleave="showInfo = false">
     <div v-if="serie.poster_path">
       <img :src="`https://image.tmdb.org/t/p/w342/${serie.poster_path}`" />
     </div>
-    <div>{{ serie.original_name }}</div>
+    <div v-if="showInfo" class="serie-info">
+      <div>{{ serie.original_name }}</div>
     <div>{{ serie.origin_country }}</div>
     <div :class="flagClassTv"></div>
+    <div class="overview-text">{{ serie.overview }}</div>
+    <div>{{ serie.first_air_date }}</div>
+    </div>
+    
     </div>
 </template>
 
@@ -31,5 +41,28 @@ flagClassTv() {
   border: 1px solid #ccc;
   padding: 10px;
   margin-bottom: 10px;
+  position: relative;
 }
+
+.serie-info {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: black;
+  display: flex;
+  flex-direction: column;
+  padding: 5px;
+  gap: 10px;
+}
+
+.overview-text {
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 8;
+  -webkit-box-orient: vertical;
+}
+
+
 </style>
