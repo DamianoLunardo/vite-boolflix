1f4aa00687b97683848daf262e09833160929559
DamianoL <damianolunardo@hotmail.it>
2023-11-13 16:05:15 +0100

    Fix hover and order in preview tv series

--- src/components/AppCardSeries.vue
+++ src/components/AppCardSeries.vue
@@ -1,7 +1,6 @@
 <script>
 export default {
   props: {
-
     serie: Object,
   },
   data() {
@@ -10,7 +9,7 @@ export default {
     };
   },
   computed: {
-flagClassTv() {
+    flagClassTv() {
       const language = this.serie.original_language;
       return `flag-icon flag-icon-${language}`;
     },
@@ -19,23 +18,36 @@ flagClassTv() {
 </script>
 
 <template>
-  <div class="card__serie"  @mouseover="showInfo = true" @mouseleave="showInfo = false">
+  <div
+    class="card__serie"
+    @mouseover="showInfo = true"
+    @mouseleave="showInfo = false"
+  >
     <div v-if="serie.poster_path">
       <img :src="`https://image.tmdb.org/t/p/w342/${serie.poster_path}`" />
     </div>
     <div v-if="showInfo" class="serie-info">
       <div>{{ serie.original_name }}</div>
-    <div>{{ serie.origin_country }}</div>
-    <div :class="flagClassTv"></div>
-    <div class="overview-text">{{ serie.overview }}</div>
-    <div>{{ serie.first_air_date }}</div>
+      <div>{{ serie.origin_country }}</div>
+      <div class="overview-text">{{ serie.overview }}</div>
+      <div :class="flagClassTv"></div>
+      <div>{{ serie.first_air_date }}</div>
+      
+      <div class="star-ratings">
+        <template v-for="i in Math.floor(serie.vote_average / 2)">
+          <i class="fas fa-star"></i>
+        </template>
+        <template v-if="serie.vote_average % 2 !== 0">
+          <i class="fas fa-star-half"></i>
+        </template>
+      </div>
     </div>
-    
-    </div>
+  </div>
 </template>
 
 <style lang="scss">
 @import url("https://cdn.jsdelivr.net/npm/flag-icon-css@3.5.0/css/flag-icon.min.css");
+@import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css");
 
 .card__serie {
   border: 1px solid #ccc;
@@ -64,5 +76,7 @@ flagClassTv() {
   -webkit-box-orient: vertical;
 }
 
-
+.star-ratings {
+  color: rgb(251, 255, 0);
+}
 </style>
