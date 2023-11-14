7d6bf31714776559f0d3aeb9db8fc866889a0519
DamianoL <damianolunardo@hotmail.it>
2023-11-14 15:57:54 +0100

    Add text for movies and series

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -6,7 +6,6 @@ import { store } from "../store";
 export default {
     components: {
         AppCard,
-    
     },
     data() {
         return {
@@ -46,27 +45,33 @@ export default {
 
     watch: {
         "store.query": function () {
-            this.fetchMovies()
-            this.fetchSeries()
-
+            this.fetchMovies();
+            this.fetchSeries();
         },
     },
-
 };
 </script>
 
 <template>
+    <h3 v-if="movies.length> 0" class="info-text">I film trovati sono:</h3>
     <div class="card-grid">
-    <AppCard v-for="movie in movies" :key="movie.id" :movie="movie" />
-    <AppCard v-for="serie in series" :key="serie.id" :serie="serie" />
+        <AppCard v-for="movie in movies" :key="movie.id" :movie="movie" />
+    </div>
+    <h3 v-if="series.length > 0" class="info-text">Le serie trovate sono:</h3>
+    <div class="card-grid">
+        <AppCard v-for="serie in series" :key="serie.id" :serie="serie" />
     </div>
 </template>
 
 <style lang="scss">
 .card-grid {
-  padding-top: 40px;
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  gap: 20px;
+    padding-top: 40px;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 20px;
+}
+
+.info-text {
+    padding-top: 20px;
 }
 </style>
